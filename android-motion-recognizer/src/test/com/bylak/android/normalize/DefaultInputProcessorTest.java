d735c427d093fed165e4387abfc5b15ac00695db
artur.bylak <artur.bylak@gmail.com>
2013-10-03 13:21:02 +0200

    Added test

--- android-motion-recognizer/src/test/com/bylak/android/normalize/DefaultInputProcessorTest.java
+++ android-motion-recognizer/src/test/com/bylak/android/normalize/DefaultInputProcessorTest.java
@@ -26,4 +26,28 @@ public class DefaultInputProcessorTest {
             Assert.assertEquals(expected[i], result[i], 0.001d);
         }
     }
+
+    @Test
+    public void testReduce() throws Exception {
+
+        //given
+        InputProcessor inputProcessor = new DefaultInputProcessor();
+        double[] expected = new double[]{0.0, 1};
+        double[] expected2 = new double[]{0.0, 0.2, 0.4, 0.6};
+
+        //when
+        double[] result = inputProcessor.processInput(2, new float[]{1, 2, 3});
+        double[] result2 = inputProcessor.processInput(4, new float[]{1, 2, 3, 4, 5, 6});
+
+        //then
+        for (int i = 0; i < expected.length; i++) {
+            Assert.assertEquals(expected[i], result[i], 0.001d);
+        }
+
+        for (int i = 0; i < result2.length; i++) {
+            Assert.assertEquals(expected2[i], result2[i], 0.001d);
+        }
+    }
+
+
 }
